46abe80ca43d205625f3804619bbc954f4024431
Matt Goldhammer <Matt.Goldhammer@gmail.com>
2022-11-25 14:54:55 -0700

    added wrapper component and skeletons for future components

---
+++ client/src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Footer() {
+  return (
+    <div>
+        {/* ADD IMAGE */}
+        <h3>Counting Sheep</h3>
+        <h5>2022</h5>
+        <img src="#" alt="logo"></img>
+    </div>
+  );
+}
---
+++ client/src/components/Nav.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+function Nav() {
+  return (
+    <nav className="navbar bg-light">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#dashboard">
+          Counting Sheep
+          <img src="#" alt="Logo"></img>
+        </a>
+        <ul className="nav">
+            {/* ADD CONDITIONAL STATEMENT SO THAT LOG OUT ONLY SHOWS WHEN AUTHENTICATED 
+            HAVE TO WAIT FOR AUTHENTICATION VARIABLES TO BE DEFINED */}
+            <li className="nav-item">
+                <a className="nav-link" href="#login"> Sign In </a>
+            </li>
+            <li className="nav-item">
+                <a className="nav-link" href="#login"> log Out </a>
+            </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
+
+export default Nav;

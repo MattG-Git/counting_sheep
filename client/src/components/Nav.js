f52dec6c268ffbfea396ba837682dd70b0bb30c5
Matt Goldhammer <Matt.Goldhammer@gmail.com>
2022-11-25 17:41:52 -0700

    installed bootstrap package and added forms

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -10,7 +10,7 @@ function Nav() {
         </a>
         <ul className="nav">
             {/* ADD CONDITIONAL STATEMENT SO THAT LOG OUT ONLY SHOWS WHEN AUTHENTICATED 
-            HAVE TO WAIT FOR AUTHENTICATION VARIABLES TO BE DEFINED */}
+            HAVE TO WAIT FOR AUTHENTICATION VARIABLES TO BE DEFINED  can be found in wk 20 activity 23*/}
             <li className="nav-item">
                 <a className="nav-link" href="#login"> Sign In </a>
             </li>

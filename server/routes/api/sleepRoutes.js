60a0c808fa7f1817c43219e0e687563724b7a7a5
Tyler M <tmorse2222@gmail.com>
2022-12-01 21:11:01 -0700

    Back end completed

--- server/routes/api/sleepRoutes.js
+++
@@ -1,6 +0,0 @@
-const router = require('express').Router();
-const { userSleepData, logSleep } = require('../../controllers/sleepController');
-
-router.route('/').get(userSleepData).post(logSleep);
-
-module.exports = router;
--- server/routes/api/userRoutes.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router();
-const { userSignUp, userLogin } = require('../../controllers/userController');
-
-router.route('/').post(userSignUp);
-router.route('/login').post(userLogin);
-
-module.exports = router;

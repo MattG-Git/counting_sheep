f59bbd15f5a34c8f2597ef5ec84c9ade7144e1cb
Tyler M <tmorse2222@gmail.com>
2022-12-05 13:13:51 -0700

    Added query to dashboard; added states to pass value to neccessary widgets

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -3,21 +3,34 @@ import HistoryWidget from "./HistoryWidget";
 import SleepInspoWidget from "./SleepInspoWidget";
 import React, { useState } from "react";
 import '../styles/style.css';
+import {useQuery} from '@apollo/client';
+import { QUERY_sleepData } from '../utils/queries';
 
 function Dashboard() {
     const [sleepAverage, setSleepAverage] = useState(0);
-  return (
-    <div>
-        {/* WOULD BE NICE TO PERSONALIZE THE HEADER WITH LOGIN NAME INSTEAD OF JUST "YOUR" */}
-        <h1>Your Counting Sheep Dashboard</h1>
-        <AvgWidget sleepAverage={sleepAverage} setSleepAverage={setSleepAverage}/>
-        <div className="d-grid gap-2 m-4">
-          <button className="btn btn-warning btn-lg custom" type="button" onClick={()=> window.location.href="/track"}>Track Sleep</button>
+    const { loading, data } = useQuery(QUERY_sleepData);
+    
+    const sleepData = data?.sleepData || [];
+    console.log(sleepData);
+    console.log(sleepAverage);
+
+    if (loading) {
+        return <div>Loading...</div>;
+    }
+
+    return (
+        <div>
+            {/* WOULD BE NICE TO PERSONALIZE THE HEADER WITH LOGIN NAME INSTEAD OF JUST "YOUR" */}
+            <h1>Your Counting Sheep Dashboard</h1>
+            <AvgWidget sleepData={sleepData} sleepAverage={sleepAverage} setSleepAverage={setSleepAverage}/>
+            <div className="d-grid gap-2 m-4">
+              <button className="btn btn-warning btn-lg custom" type="button" onClick={()=> window.location.href="/track"}>Track Sleep</button>
+            </div>
+            {/* Pass sleep data to history widget */}
+            <HistoryWidget sleepData={sleepData} sleepAverage={sleepAverage} setSleepAverage={setSleepAverage}/>
+            <SleepInspoWidget />
         </div>
-        <HistoryWidget />
-        <SleepInspoWidget />
-    </div>
-  )
+    );
 }
 
 export default Dashboard;

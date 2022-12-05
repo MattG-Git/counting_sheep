699cdfe9529ba86aa960da150c1669bac3cb3b2b
Tyler M <tmorse2222@gmail.com>
2022-12-04 17:22:32 -0700

    Code refactored to automatically start app in legacy mode, background image moved to assets folder within src directory; login page refactored to create queries and mutations

--- client/src/App.js
+++ client/src/App.js
@@ -6,29 +6,37 @@ import Dashboard from "./components/Dashboard";
 import TrackForm from "./pages/TrackForm";
 import LoginForm from "./pages/LoginForm";
 import './App.css';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: '/graphql',
+  cache: new InMemoryCache()
+});
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Nav />
-        <Routes>
-          <Route 
-            path="/"
-            element={<Dashboard />}
-          />
-          <Route
-            path="/login"
-            element={<LoginForm />}
-          />
-          <Route
-            path="/track"
-            element={<TrackForm />}
-          />
-        </Routes>
-        <Footer />
-      </div>
-    </Router>
+    <ApolloProvider client={client}>
+      <Router>
+        <div className="App">
+          <Nav />
+          <Routes>
+            <Route 
+              path="/"
+              element={<Dashboard />}
+            />
+            <Route
+              path="/login"
+              element={<LoginForm />}
+            />
+            <Route
+              path="/track"
+              element={<TrackForm />}
+            />
+          </Routes>
+          <Footer />
+        </div>
+      </Router>
+    </ApolloProvider>
   );
 }
 

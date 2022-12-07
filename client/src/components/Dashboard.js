a735ff18a799b6617389885a9e39a5b14faf3391
Tyler M <tmorse2222@gmail.com>
2022-12-06 17:30:10 -0700

    Added average hours slept

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -7,27 +7,34 @@ import {useQuery} from '@apollo/client';
 import { QUERY_sleepData } from '../utils/queries';
 
 function Dashboard() {
-    const [sleepAverage, setSleepAverage] = useState(0);
     const { loading, data } = useQuery(QUERY_sleepData);
     
     const sleepData = data?.sleepData || [];
-    console.log(sleepData);
-    console.log(sleepAverage);
 
     if (loading) {
         return <div>Loading...</div>;
     }
 
+    const sleepHours = sleepData.map((sleep) => sleep.hours);
+
+    const sleepHoursSum = sleepHours.reduce((a, b) => a + b, 0);
+
+    const sleepHoursAvg = sleepHoursSum / sleepHours.length;
+
+    const sleepHoursAvgRounded = Math.round(sleepHoursAvg * 10) / 10;
+
+    console.log(sleepHoursAvgRounded);
+    console.log(sleepData);
     return (
         <div>
             {/* WOULD BE NICE TO PERSONALIZE THE HEADER WITH LOGIN NAME INSTEAD OF JUST "YOUR" */}
             <h1>Your Counting Sheep Dashboard</h1>
-            <AvgWidget sleepData={sleepData} sleepAverage={sleepAverage} setSleepAverage={setSleepAverage}/>
+            <AvgWidget sleepAverage={sleepHoursAvgRounded}/>
             <div className="d-grid gap-2 m-4">
               <button className="btn btn-warning btn-lg custom" type="button" onClick={()=> window.location.href="/track"}>Track Sleep</button>
             </div>
             {/* Pass sleep data to history widget */}
-            <HistoryWidget sleepData={sleepData} sleepAverage={sleepAverage} setSleepAverage={setSleepAverage}/>
+            <HistoryWidget sleepData={sleepData}/>
             <SleepInspoWidget />
         </div>
     );
